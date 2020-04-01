79c42bb842afd0e9b23df72d2655aa4db8f32871
Vildan Softic <zewa666@gmail.com>
2020-04-01 12:52:20 +0200

    load flash demo

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,15 @@
+
+
 export class App {
   public message: string = "Hello Aurelia from Electron!";
+  public flashdemo: string;
+
+  constructor() {
+    const { remote } = (window as any).electronRequire('electron');
+    const path = (window as any).electronRequire('path');
+
+    this.flashdemo = remote.process.env.NODE_ENV === "development"
+      ? "./build/assets/flashdemo.swf"
+      : path.join(path.dirname(remote.app.getPath('exe')), "./build/assets/flashdemo.swf");
+  }
 }
